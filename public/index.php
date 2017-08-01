998f24f3c7f02ad1a5edace5d6c2936731b31035
Kamil Pasterczyk <yiome@outlook.com>
2017-08-01 10:37:22 +0200

    domian update

--- public/index.php
+++ public/index.php
@@ -2,11 +2,11 @@
     $lang = @substr(Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']), 0, 2);
     switch($lang){
         case "pl":
-            header("Location: pl/card");
+            header("Location: pl/card.html");
         break;
 
         default:
-            header("Location: eng/card");
+            header("Location: eng/card.html");
         break;
     }
 ?>
